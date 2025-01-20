9a57aef124939caa39793a98290b0a1a528aaaa4
Shtain3r <147108127+Shtain3r@users.noreply.github.com>
2025-01-20 14:14:43 +0200

    Added Campfire timer Logic with 2 steps

--- client/services/campfire.lua
+++ client/services/campfire.lua
@@ -1,3 +1,10 @@
+local campfireDurationSeconds = 60 -- Time before the campfire changes to "no fire" (in seconds)
+local noFireDurationSeconds = 30 -- Time before the "no fire" campfire disappears (in seconds)
+
+-- Convert durations to milliseconds
+local campfireDuration = campfireDurationSeconds * 1000
+local noFireDuration = noFireDurationSeconds * 1000
+
 local campfire = 0
 local progressbar = exports.vorp_progressbar:initiate()
 
@@ -22,6 +29,39 @@ local function placeCampfire()
         SetEntityHeading(prop, GetEntityHeading(PlayerPedId()))
         PlaceObjectOnGroundProperly(prop, false)
         campfire = prop
+
+        -- Start timer to replace campfire with "no fire" version
+        Citizen.CreateThread(function()
+            Wait(campfireDuration)
+            if DoesEntityExist(campfire) then
+                local campfireCoords = GetEntityCoords(campfire)
+                local campfireHeading = GetEntityHeading(campfire)
+                SetEntityAsMissionEntity(campfire, false, false)
+                DeleteObject(campfire)
+
+                -- Spawn the "no fire" campfire prop
+                RequestModel("p_campfire03x_nofire", false)
+                repeat Wait(0) until HasModelLoaded("p_campfire03x_nofire")
+                local noFireProp = CreateObject(GetHashKey("p_campfire03x_nofire"), campfireCoords.x, campfireCoords.y, campfireCoords.z, true, false, false, false, false)
+                SetEntityHeading(noFireProp, campfireHeading)
+                PlaceObjectOnGroundProperly(noFireProp, false)
+                campfire = noFireProp
+
+                -- Notification for the player
+                TriggerEvent("vorp:TipRight", _U('CampfireChanged'), 5000)
+
+                -- Start timer to remove the "no fire" prop
+                Citizen.CreateThread(function()
+                    Wait(noFireDuration)
+                    if DoesEntityExist(campfire) then
+                        SetEntityAsMissionEntity(campfire, false, false)
+                        DeleteObject(campfire)
+                        campfire = 0
+                        TriggerEvent("vorp:TipRight", _U('CampfireRemoved'), 5000)
+                    end
+                end)
+            end
+        end)
     end)
 end
 
